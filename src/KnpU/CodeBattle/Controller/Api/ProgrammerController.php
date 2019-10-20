eb6946dcb22a2a88e52e6981148b9e69c79a212e
WENI HSU <hsu.weni@gmail.com>
2019-10-20 14:54:38 +0800

    Chapter 10: Fixing the Content-Type on POST

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -36,8 +36,10 @@ class ProgrammerController extends BaseController
            'nickname' => $programmer->nickname,
         ));
 
-        $response = new Response('It worked! Trust me, I\'m an API!', 201);
+        $data = $this->serializeProgrammer($programmer);
+        $response = new JsonResponse($data, 201);
         $response->headers->set('Location', $url);
+
         return $response;
     }
 
@@ -51,9 +53,7 @@ class ProgrammerController extends BaseController
         }
 
         $data = $this->serializeProgrammer($programmer);
-
-        $response = new Response(json_encode($data), 200);
-        $response->headers->set('Content-Type', 'application/json');
+        $response = new JsonResponse($data, 200);
 
         return $response;
     }
@@ -67,8 +67,7 @@ class ProgrammerController extends BaseController
             $data['programmers'][] = $this->serializeProgrammer($programmer);
         }
 
-        $response = new Response(json_encode($data), 200);
-        $response->headers->set('Content-Type', 'application/json');
+        $response = new JsonResponse($data, 200);
 
         return $response;
     }
